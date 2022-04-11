47806c4f2c44210cc7fd1fcd3509cbe160abc612
Kerollos <kerolloswageeh03@gmail.com>
2022-04-11 11:35:48 +0200

    tokenization done

--- tokenization.py
+++ tokenization.py
@@ -4,6 +4,7 @@ x = input()
 y = re.search('^(IF [0-9]+ THEN ([_a-zA-Z][_a-zA-Z0-9]* := ([_a-zA-Z]+[_a-zA-Z0-9]*|[0-9]+);)+'
               '(ELSE IF NUM THEN ([_a-zA-Z][_a-zA-Z0-9]* := ([_a-zA-Z][_a-zA-Z0-9]*|[0-9]+);)+)*'
               '(ELSE ([_a-zA-Z][_a-zA-Z0-9]* := ([_a-zA-Z][_a-zA-Z0-9]*|[0-9]+);)+)? END)+$', x)
+
 if y is not None:
     print(f'The recognized expression: "{y.string}"')
     print(f"Its tokens are:")
