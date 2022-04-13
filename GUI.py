825a414deb6fc298516baed2081af7b777c26b23
mostafanasrat4 <101895654+mostafanasrat4@users.noreply.github.com>
2022-04-13 09:01:21 +0200

    Added PyQt5 GUI

---
+++ GUI.py
@@ -0,0 +1,127 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from tokenization import get_tokens_list
+from plot import tiny_transitions
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1023, 610)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(670, 60, 141, 51))
+        self.pushButton.setObjectName("pushButton")
+        self.plainTextEdit = QtWidgets.QPlainTextEdit(self.centralwidget)
+        self.plainTextEdit.setGeometry(QtCore.QRect(240, 20, 391, 131))
+        self.plainTextEdit.setObjectName("plainTextEdit")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(80, 60, 151, 41))
+        self.label.setObjectName("label")
+        self.tableWidget = QtWidgets.QTableWidget(self.centralwidget)
+        self.tableWidget.setGeometry(QtCore.QRect(10, 210, 361, 321))
+        self.tableWidget.setObjectName("tableWidget")
+        self.tableWidget.setColumnCount(4)
+        self.tableWidget.setRowCount(0)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(0, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(1, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(2, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(3, item)
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(30, 180, 81, 31))
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(450, 180, 51, 31))
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(670, 120, 261, 41))
+        self.label_4.setObjectName("label_4")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1023, 26))
+        self.menubar.setObjectName("menubar")
+        self.menuHome = QtWidgets.QMenu(self.menubar)
+        self.menuHome.setObjectName("menuHome")
+        self.menuAbout = QtWidgets.QMenu(self.menubar)
+        self.menuAbout.setObjectName("menuAbout")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.menubar.addAction(self.menuHome.menuAction())
+        self.menubar.addAction(self.menuAbout.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "TINY Language Compiler"))
+        self.pushButton.setText(_translate("MainWindow", "Tokenize Code"))
+        self.pushButton.clicked.connect(self.onClickTokenize)
+        self.label.setText(_translate("MainWindow", "Insert your code here:"))
+        self.plainTextEdit.setPlaceholderText(
+        """Example:
+            IF 1 THEN
+            x := y;
+            ELSE IF 2 THEN
+            x := z;
+            ELSE
+            x:= 0;
+            END""")
+        item = self.tableWidget.horizontalHeaderItem(0)
+        item.setText(_translate("MainWindow", "Token"))
+        item = self.tableWidget.horizontalHeaderItem(1)
+        item.setText(_translate("MainWindow", "Type"))
+        item = self.tableWidget.horizontalHeaderItem(2)
+        item.setText(_translate("MainWindow", "Current State"))
+        item = self.tableWidget.horizontalHeaderItem(3)
+        item.setText(_translate("MainWindow", "Next State"))
+        self.label_2.setText(_translate("MainWindow", "Tokens List:"))
+        self.label_3.setText(_translate("MainWindow", "DFA:"))
+        self.label_4.setText(_translate("MainWindow",u"<html><head/><body><h2><span style=\" color:#ff0000;\">Invalid IF statement!!!</span></h2></body></html>"))
+        self.label_4.hide()
+        self.menuHome.setTitle(_translate("MainWindow", "Home"))
+        self.menuAbout.setTitle(_translate("MainWindow", "About"))
+
+    def onClickTokenize(self):
+        input_code = str(self.plainTextEdit.toPlainText())
+        tokens = self.get_tokens_tabledata(input_code)
+        if tokens is None:
+            self.label_4.show()
+            self.tableWidget.setRowCount(0)
+        else:
+            self.label_4.hide()
+            self.tableWidget.setRowCount(len(tokens))
+            row = 0
+            for token in tokens:
+                self.tableWidget.setItem(row, 0, QtWidgets.QTableWidgetItem(token["token"]))
+                self.tableWidget.setItem(row, 1, QtWidgets.QTableWidgetItem(token["type"]))
+                self.tableWidget.setItem(row, 2, QtWidgets.QTableWidgetItem(token["current"]))
+                self.tableWidget.setItem(row, 3, QtWidgets.QTableWidgetItem(token["next"]))
+                row = row + 1
+
+    def get_tokens_tabledata(self, input_code):
+        tokens_list = get_tokens_list(input_code)
+        if tokens_list is None:
+            return None
+        else:
+            current = '1'
+            for token in tokens_list:
+                token["current"] = current
+                next = tiny_transitions[current][token["type"]]
+                token["next"] = next
+                current = next
+            return tokens_list
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
--- tokenization.py
+++ tokenization.py
@@ -1,16 +1,54 @@
+# import re
+#
+# x = input()
+# y = re.search('^(IF [0-9]+ THEN( [_a-zA-Z][_a-zA-Z0-9]* := ([_a-zA-Z]+[_a-zA-Z0-9]*|[0-9]+);)+'
+#               '( ELSE IF NUM THEN ([_a-zA-Z][_a-zA-Z0-9]* := ([_a-zA-Z][_a-zA-Z0-9]|[0-9]+);)+)*'
+#               '( ELSE ([_a-zA-Z][_a-zA-Z0-9]* := ([_a-zA-Z][_a-zA-Z0-9]*|[0-9]+);)+)? END)+$', x)
+#
+# if y is not None:
+#     print(f'The recognized expression: "{y.string}"')
+#     print(f"Its tokens are:")
+#     tokens = re.findall('IF|[0-9]+|THEN|ELSE|END|[_a-zA-Z][_a-zA-Z0-9]*|:=|;', y.string)
+#     for i in range(len(tokens)):
+#         print(f'{i + 1} : {tokens[i]}')
+#
+# else :
+#     print('Invalid Input')
 import re
 
-x = input()
-y = re.search('^(IF [0-9]+ THEN( [_a-zA-Z][_a-zA-Z0-9]* := ([_a-zA-Z]+[_a-zA-Z0-9]*|[0-9]+);)+'
-              '( ELSE IF NUM THEN ([_a-zA-Z][_a-zA-Z0-9]* := ([_a-zA-Z][_a-zA-Z0-9]|[0-9]+);)+)*'
-              '( ELSE ([_a-zA-Z][_a-zA-Z0-9]* := ([_a-zA-Z][_a-zA-Z0-9]*|[0-9]+);)+)? END)+$', x)
 
-if y is not None:
-    print(f'The recognized expression: "{y.string}"')
-    print(f"Its tokens are:")
-    tokens = re.findall('IF|[0-9]+|THEN|ELSE|END|[_a-zA-Z][_a-zA-Z0-9]*|:=|;', y.string)
-    for i in range(len(tokens)):
-        print(f'{i + 1} : {tokens[i]}')
+def get_tokens_list(input_code):
+    y = re.search('^(IF [0-9]+ THEN( [_a-zA-Z][_a-zA-Z0-9]* := ([_a-zA-Z]+[_a-zA-Z0-9]*|[0-9]+);)+'
+                  '( ELSE IF NUM THEN ([_a-zA-Z][_a-zA-Z0-9]* := ([_a-zA-Z][_a-zA-Z0-9]|[0-9]+);)+)*'
+                  '( ELSE ([_a-zA-Z][_a-zA-Z0-9]* := ([_a-zA-Z][_a-zA-Z0-9]*|[0-9]+);)+)? END)+$', input_code)
 
-else :
-    print('Invalid Input')
+    if y is not None:
+        #print(f'The recognized expression: "{y.string}"')
+        #print(f"Its tokens are:")
+        tokens_list = []
+        tokens = re.findall('IF|[0-9]+|THEN|ELSE|END|[_a-zA-Z][_a-zA-Z0-9]*|:=|;', y.string)
+        for token in tokens:
+            if token == "IF":
+                tokens_list.append({"token": token, "type": "IF"})
+            elif token == "THEN":
+                tokens_list.append({"token": token, "type": "THEN"})
+            elif token == "ELSE":
+                tokens_list.append({"token": token, "type": "ELSE"})
+            elif token == "END":
+                tokens_list.append({"token": token, "type": "END"})
+            elif token == ":=":
+                tokens_list.append({"token": token, "type": ":="})
+            elif token == ";":
+                tokens_list.append({"token": token, "type": ";"})
+            elif re.search("[0-9]+", token) is not None:
+                tokens_list.append({"token": token, "type": "NUM"})
+            elif re.search("[_a-zA-Z][_a-zA-Z0-9]*", token) is not None:
+                tokens_list.append({"token": token, "type": "ID"})
+            else:
+                raise Exception("Invalid token returned from tokenization")
+
+        return tokens_list
+
+    else:
+        print("Invalid IF statement")
+        return None
